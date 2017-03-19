ea5b23773b6292dc4efb8840943ec7570ac24fe1
Mihai Bucur <mihaibucur@users.noreply.github.com>
2017-03-20 00:35:06 +0200

    update main.js db calculator

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
-console.log('This would be the main JS file.');
+ $('input[name=referencePowLev], input[name=measuredPowLev]').keyup(function() {
+    var divParent = $(this).closest('div');
+    var referencePowLev = $('input[name=referencePowLev]', divParent).val()-0;
+    var measuredPowLev = $('input[name=measuredPowLev]', divParent).val()-0;
+    if (referencePowLev >= 0 && measuredPowLev >= 0)
+        $('span.dBresult', divParent).text(10*Math.log10(measuredPowLev/referencePowLev));
+});
